2d3f706068cbc60e555cf57f69659def4920fe23
cmade <cmangerere@gmail.com>
2020-11-28 17:00:03 -0500

    Completed Week2_HW2_Functions.js

--- JavaScript_HW/Week2_HW2_Functions.js
+++ JavaScript_HW/Week2_HW2_Functions.js
@@ -19,20 +19,96 @@ function isCharacterAVowel(x) {
   }
 }
 // Define a function sumArray and a function multiplyArray that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sumArray([1,2,3,4]) should return 10, and multiplyArray([1,2,3,4]) should return 24.
+var sum = 0;
 function sumArray(arr) {
-  
+  arr.forEach(element => {
+    sum += element;
+  });
+  return sum;
 }
+console.log(`This is a sum of an array: ${sumArray([1, 2, 3, 4])}`);
+var multiple = 1;
+function multiplyArray(arr) {
+  arr.forEach(element => {
+    multiple *= element;
+  });
+  return multiple;
+}
+console.log(`This is a multiple of an array: ${multiplyArray([1, 2, 3, 4])}`);
 // Write a function that returns the number of arguments passed to the function when called.
+function numOfArg(...args) {
+  return args.length;
+}
+console.log(`These are the number of arguments: ${numOfArg(1, 2, 3, 4, 5)}`);
 // Define a function reverseString that computes the reversal of a string. For example, reverseString("jag testar") should return the string "ratset gaj".
+var newArr = [];
+function reverseString(x) {
+   x = x.split("")
+   x.forEach(element => {
+    newArr.unshift(element);
+  });
+  return newArr.join("");
+}
+console.log(`These are the number of arguments: ${reverseString("jag testar")}`);
 // Write a function findLongestWord that takes an array of words and returns the length of the longest one.
+var newElement = 0;
+function findLongestWord(...word) {
+  word.forEach(element => {
+    for (let index = 0; index < element.length; index++) {
+      if(element[index].length > newElement){
+        newElement = element[index].length
+      }
+    }
+  });
+  return newElement;
+}
+console.log(`Find the longest word: ${findLongestWord(["Hello", "Bob", "Okay", "Toddler"])}`);
 // Write a function filterLongWords that takes an array of words and a number i and returns the array of words that are longer than i characters long.
+var newElement2 = [];
+function filterLongWords(arr, i) {
+    arr.filter((element) => {
+      if (element.length > i) {
+        newElement2.push(element);
+      }
+    })
+    return newElement2;
+}
+console.log(`Filter the longest words: ${filterLongWords(["Hello", "Bob", "Okay", "Toddler"], 3)}`);
 // Bonus
 
-// Add a method reverseString (from question 6) to the object String so that it is possible to call: "Per Scholas".reverseString().
-// Write a function that takes a string as argument and returns an object where:
+// 1. Add a method reverseString (from question 6) to the object String so that it is possible to call: "Per Scholas".reverseString().
+var newArr2 = [];
+String.prototype.reverseString = function( ) {
+    str = String(this)
+    str = str.split("")
+    str.forEach(element => {
+    newArr2.unshift(element);
+ });
+ return newArr2.join("");
+}
+console.log(`Call the reverseString method on the word "Per Scholas": ${"Per Scholas".reverseString()}`);
+// 2. Write a function that takes a string as argument and returns an object where:
 // the keys are the characters that occur in the string
 // the values are the number of occurrences for each letter, regardless of the case
 // For example, calling the function with the string "Per Scholas" will return:
+
+function obj(str) {
+  let newObj = {};
+  str = str.toLowerCase();
+  var strArr = str.split(" ").join("").split("");
+  for (let index = 0; index < str.length; index++) {
+    if (str[index] !== " ") {
+      newObj[str[index]] = 0;
+      strArr.forEach(element => {
+        if (element.match(str[index])) {
+          newObj[str[index]] += 1;
+        }
+      });
+    }
+  }
+  return newObj; ;
+}
+console.log(`This is an object: ${JSON.stringify(obj("Per Scholas"))}`);
 
 // {
 //   a: 1,
