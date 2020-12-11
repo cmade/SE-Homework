4ed3b0827081437554e5b9005e65cfa0a00b6b2b
cmade <cmangerere@gmail.com>
2020-12-11 00:11:27 -0500

    Completed Week3_Day4_Morning and Afternoon Lab

---
+++ Javascript_Labs/Week3_Day4_AfternoonLab.js
@@ -0,0 +1,52 @@
+// Create an Animal class.
+// The class will have name, age, color, legs, superPower attributes
+// Create two objects of this class - both with a unique superPower
+// create three methods for this class - include one method named userSuperPower()
+// Create two objects for this class
+// Create a class name User
+// This class with have firstName, lastName, department attributes
+// a method name changeDepartment()
+// Create three instances of this class
+// Change department for two of the instances
+// Create three objects for this class
+class Animal {
+  constructor(name, age, color, legs, superPower) {
+    this.name = name;
+    this.age = age;
+    this.color = color;
+    this.legs = legs;
+    this.superPower = superPower;
+  }
+  userSuperPower() {
+    return `${this.name} can ${this.superPower}`;
+  }
+  run() {
+    return `${this.name} can run`;
+  }
+  walk() {
+    return `${this.name} can walk`;
+  }
+}
+let rabbit = new Animal('Bob', 29, 'green', 2, 'spit fire');
+console.log(rabbit.userSuperPower());
+let t1 = new Animal('Tim', 49, 'blue', 1, 'throw ice');
+console.log(t1.userSuperPower());
+let bean = new Animal('Cathy', 59, 'orang', 3, 'shoot lasers');
+console.log(bean.userSuperPower());
+
+class User {
+  constructor(firstName, lastName, department) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.department = department;
+  }
+  changeDepartment() {
+    return `${this.firstName} ${this.lastName} is from ${this.department}'s new departiment`;
+  }
+}
+let user1 = new User('Jeff', 'Zip', 'hr');
+console.log(user1.changeDepartment());
+let user2 = new User('Lisa', 'Dip', 'accounting');
+console.log(user2.changeDepartment());
+let user3 = new User('Bart', 'Drop', 'art');
+console.log(user3.changeDepartment());
---
+++ Javascript_Labs/Week3_Day4_MorningLab.js
@@ -0,0 +1,29 @@
+// OOP - Inheritance
+
+// Create a class called Fish
+// Define a method Swim( )
+// Create a class Nemo that inherits from Fish
+// Define an attribute color
+// Create a Nemo object. Can it swim?
+// Find the difference between abstract class and an interface
+// Which of the two can have implementation of a method in the class?
+//Subclass can implement a method.
+
+class Fish {
+  constructor(name) {
+    this.name = name;
+  }
+  swim() {
+    return `${this.name} can swim`;
+  }
+}
+class Nemo extends Fish {
+  constructor(name, color) {
+    super(name);
+    this.color = color;
+  }
+}
+
+var nemo = new Nemo('nemo', 'green');
+
+console.log(nemo.swim() + ' and is ' + nemo.color);
