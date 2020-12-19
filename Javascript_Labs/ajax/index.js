02d9efa772f790fb4b2c2cde02aeaa505c5e735b
cmade <cmangerere@gmail.com>
2020-12-18 22:22:15 -0500

    Completed todays lab

---
+++ Javascript_Labs/ajax/index.js
@@ -0,0 +1,21 @@
+$(() => {
+  $('form').on('submit', (e) => {
+    e.preventDefault();
+
+    const userInput = $('input[type="text"]').val();
+
+    $.ajax({
+      url: 'http://www.omdbapi.com/?apikey=53aa2cd6&t=' + userInput,
+    }).then(
+      (data) => {
+        console.log(data);
+        $('#title').html(data.Title);
+        $('#year').html(data.Year);
+        $('#rated').html(data.Rated);
+      },
+      () => {
+        console.log('Bad Request');
+      }
+    );
+  });
+});
