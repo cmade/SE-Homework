dead437da8e9d11d8afbe41046d6808d72924a12
cmade <cmangerere@gmail.com>
2020-12-30 00:49:12 -0500

    Created array for to store 6 alien objects

---
+++ JavaScript_HW/spacebattle/app.js
@@ -0,0 +1,2 @@
+//Define an array six Aliens
+const alienArray = [];
--- Javascript_Labs/spacebattle/app.js
+++
@@ -1,4 +0,0 @@
-let action = null;
-while (action !== 'dance') {
-  action = prompt('What do you want to do', 'Hint: Move to music');
-}
