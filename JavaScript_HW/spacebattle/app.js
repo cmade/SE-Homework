604570bfd5f6cac0d7046c6962bac7b1296cc055
cmade <cmangerere@gmail.com>
2020-12-31 15:23:11 -0500

    Defined an actor class for the game

--- JavaScript_HW/spacebattle/app.js
+++ JavaScript_HW/spacebattle/app.js
@@ -1,4 +1,47 @@
 //Define an array six Aliens
 const alienArray = [];
 //Count the number of Aliens
-var i = 0;
+var alienCount = 0;
+//Define an actor class for the game
+class Actor {
+  constructor(name, hull, firepower, accuracy) {
+    this.name = name;
+    this.hull = hull;
+    this.firepower = firepower;
+    this.accuracy = accuracy;
+  }
+  attack() {
+    if (alienCount >= 6) {
+      $('#game-text').append('');
+      $('#game-text').append(
+        `<div class="action">You won! ${spaceShip.name} has defeated the aliens!<br></div>`
+      );
+    } else {
+      if (Math.random() < spaceShip.accuracy) {
+        alienArray[alienCount].hull =
+          alienArray[alienCount].hull - spaceShip.firepower;
+        if (alienArray[alienCount].hull <= 0) {
+          $('#game-text').append(
+            `<div class="action">${alienArray[alienCount].name} ${
+              alienCount + 1
+            } is destroyed<br></div>`
+          );
+          i++;
+        } else {
+          alienAttack();
+        }
+      } else {
+        $('#game-text').append(
+          `<div class="action">${spaceShip.name} has missed...<br></div>`
+        );
+        alienAttack();
+      }
+    }
+  }
+  retreat() {
+    $('#game-text').append(
+      `<div class="action">${this.name} has retreated! Game over!</div>`
+    );
+    var alienCount = 0;
+  }
+}
