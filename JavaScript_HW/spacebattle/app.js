b324744740a3f9d6a588afd64f888f15df02cc6c
cmade <cmangerere@gmail.com>
2021-01-02 15:02:16 -0500

    Define six aliens

--- JavaScript_HW/spacebattle/app.js
+++ JavaScript_HW/spacebattle/app.js
@@ -45,10 +45,46 @@ class Actor {
     var alienCount = 0;
   }
 }
-//Define six aliens
+//Define alien attack function
 function alienAttack() {
   $('#game-text').append(
-    `<div class="action">${alienArray[alienCount].name} ${alienCount + 1}'s health is ${
-      alienArray[alienCount].hull
-    }<br></div>`
+    `<div class="action">${alienArray[alienCount].name} ${
+      alienCount + 1
+    }'s health is ${alienArray[alienCount].hull}<br></div>`
   );
+  setTimeout(function () {
+    $('#game-text').append(
+      `<div class="action">${alienArray[alienCount].name} ${
+        i + 1
+      } is launching an attack...<br></div>`
+    );
+    if (Math.random() < alienArray[alienCount].accuracy) {
+      spaceShip.hull = spaceShip.hull - alienArray[alienCount].firepower;
+      setTimeout(function () {
+        $('#game-text').append(
+          `<div class="action">${spaceShip.name} has been hit! Health is now at ${spaceShip.hull}<br></div>`
+        );
+      }, 3000);
+    } else {
+      $('#game-text').append(
+        `<div class="action">${alienArray[alienCount].name} ${
+          i + 1
+        } has missed...<br></div>`
+      );
+    }
+  }, 2000);
+  if (spaceShip.hull <= 0) {
+    $('#game-text').append(`${spaceShip.name} is dead! Game Over`);
+  }
+}
+//Define six aliens
+for (let i = 0; i < 6; i++) {
+  let alien = new Actor(
+    'Alien',
+    getRand(3, 7),
+    getRand(2, 4),
+    Number(getRandomDec(0.6, 0.8).toFixed(1)),
+    6
+  );
+  alienArray.push(alien);
+}
